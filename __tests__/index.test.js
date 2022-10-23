d16608df90356f24f049627861793d7011925f78
lebedev_alex <lebedevalex3@gmail.com>
2022-10-23 15:17:20 +0300

    Fix tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,50 +1,25 @@
 import fs from 'fs';
 import { fileURLToPath } from 'url';
 import path, { dirname } from 'path';
-import diff from '../index.js';
+import genDiff from '../index.js';
 
-const filename = fileURLToPath(import.meta.url);
-const dir = dirname(filename);
+const dir = dirname(fileURLToPath(import.meta.url));
 
 const getFixturePath = (file) => path.join(dir, '..', '__fixtures__', file);
+const readFixture = (file) => fs.readFileSync(getFixturePath(file), 'utf8');
 
-const variants = [
-  {
-    resultPath: 'stylish_result.txt',
-    type: 'json',
-    format: 'stylish',
-  },
-  {
-    resultPath: 'stylish_result.txt',
-    type: 'yaml',
-    format: 'stylish',
-  },
-  {
-    resultPath: 'plain_result.txt',
-    type: 'json',
-    format: 'plain',
-  },
-  {
-    resultPath: 'json_result.txt',
-    type: 'json',
-    format: 'json',
-  },
-  {
-    resultPath: 'stylish_result.txt',
-    type: 'json',
-    format: null,
-  },
-];
+const stylishResult = readFixture('stylish_result.txt');
+const plainResult = readFixture('plain_result.txt');
+const jsonResult = readFixture('json_result.txt');
 
-test.each(variants)(
-  'gendiff works correct for $type with $format',
-  ({
-    resultPath, format, type,
-  }) => {
-    const file1 = getFixturePath(`file1.${type}`);
-    const file2 = getFixturePath(`file2.${type}`);
-    const expected = fs.readFileSync(getFixturePath(resultPath), 'utf8');
-    const result = format ? diff(file1, file2, format) : diff(file1, file2);
-    expect(result).toEqual(expected);
-  },
-);
+describe('gendiff', () => {
+  test.each(['yaml', 'json'])('gendiff %s', (format) => {
+    const filepath1 = getFixturePath(`file1.${format}`);
+    const filepath2 = getFixturePath(`file2.${format}`);
+
+    expect(genDiff(filepath1, filepath2)).toEqual(stylishResult);
+    expect(genDiff(filepath1, filepath2, 'stylish')).toEqual(stylishResult);
+    expect(genDiff(filepath1, filepath2, 'plain')).toEqual(plainResult);
+    expect(genDiff(filepath1, filepath2, 'json')).toEqual(jsonResult);
+  });
+});
